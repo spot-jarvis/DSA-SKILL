2588d493a4dfb77415530c88bced1205c93a78e3
spot-jarvis <shefinsharafudheen.s@gmail.com>
2025-11-26 07:27:37 +0530

    Reorder list optimal approach

--- Linked List/ReorderList.py
+++ Linked List/ReorderList.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+#BRUTE FORCE APPROACH HAVING O(N) BOTH SPACE AND TIME
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
@@ -26,3 +27,34 @@ class Solution:
             j -=1
 
         nodes[i].next = None
+#OPTIMAL 
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        #for finding the middle 
+        slow,fast = head,head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        # reversing the second list
+
+        second = slow.next
+        prev =slow.next = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+        #merge two halfes
+
+        first ,second = head,prev
+        while second:
+            temp1,temp2 = first.next ,second.next
+            first.next = second
+            second.next = temp1
+            first,second = temp1,temp2
+
+
+
+
+        
