033f338cf1e10a98837a3a638a1034a9b8dc0e82
spot-jarvis <shefinsharafudheen.s@gmail.com>
2025-11-29 07:33:46 +0530

    Find first and last position of element in sorted array

---
+++ BinarySearch/FirstandLastPosition.py
@@ -0,0 +1,27 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        low = 0
+        high = n-1
+        first = last = -1
+        while low <= high :
+            mid = ( low + high )//2
+            if nums[mid] == target:
+                first = mid
+                high = mid -1
+            elif nums[mid] < target:
+                low = mid +1
+            else :
+                high  = mid -1
+
+        low,high = 0 , n-1
+        while low <= high:
+            mid = (low + high) //2
+            if nums[mid] == target:
+                last = mid
+                low = mid +1
+            elif nums[mid] < target :
+                low = mid +1
+            else:
+                high = mid -1
+        return [first,last]
