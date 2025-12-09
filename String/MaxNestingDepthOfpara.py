18cbc2a902b9e45f37207d3562213fd4a2f76a76
spot-jarvis <shefinsharafudheen.s@gmail.com>
2025-12-09 06:38:43 +0530

    maximum nesting depth of the paranthesis

---
+++ String/MaxNestingDepthOfpara.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        res = 0
+        count = 0
+        for char in s:
+            if char =='(':
+                count +=1
+                res = max(res,count)
+            elif char ==  ')' :
+                count -=1
+        return res
