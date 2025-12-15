9383b8029658d06f23935b2aa4c8647f7432e726
spot-jarvis <shefinsharafudheen.s@gmail.com>
2025-12-15 07:16:50 +0530

    Finding pivot index optimal using prefixsum

--- Array/PivotIndex.py
+++ Array/PivotIndex.py
@@ -13,4 +13,15 @@ class Solution:
             if rightSum == leftSum :
                 return i
         return -1 
-    
+    #optimal approach using prefixSum 
+
+    class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        totalSum = sum(nums)
+        leftSum = 0
+        for i in range(len(nums)):
+            rightSum = totalSum - nums[i]-leftSum
+            if rightSum == leftSum :
+                return i
+            leftSum += nums[i]
+        return -1
