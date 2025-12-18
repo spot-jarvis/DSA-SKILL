969c377ce6bfd68d92d84a25de24009599556e8e
spot-jarvis <shefinsharafudheen.s@gmail.com>
2025-12-18 07:14:59 +0530

    reverselist using recursion

---
+++ prefix/RangesumQuery2D.py
@@ -0,0 +1,13 @@
+#Brute Force approach 
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix = matrix
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        res = 0
+        for r in range(row1,row2 + 1):
+            for c in range(col1,col2+1):
+                res += self.matrix[r][c]
+        return res        
+
