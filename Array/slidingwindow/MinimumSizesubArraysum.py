554764e6253e30843a91780e76d6cae1668a2b93
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-01-01 07:21:52 +0530

    find the minimum subarray sum using variable sliding window

---
+++ Array/slidingwindow/MinimumSizesubArraysum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        totalSum = left = 0
+        res = float('inf')
+
+        for right in range(len(nums)):
+            totalSum += nums[right]
+            while totalSum >= target:
+                res = min(right - left +1, res)
+                totalSum -= nums[left]
+                left += 1
+        return 0 if res == float('inf') else res
+    
