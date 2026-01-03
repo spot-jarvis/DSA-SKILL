7c0c78e4cf2c8fdc3b406a5cb3d41a0bf875cbaa
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-01-03 08:51:35 +0530

    finding the path using bfs

--- trees/PathSum.py
+++ trees/PathSum.py
@@ -18,3 +18,29 @@ class Solution:
             return dfs(node.left, currsum) or dfs(node.right, currsum)
 
         return dfs(root,0)
+#using bfs 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root :
+            return False
+
+        queue = deque([(root,targetSum - root.val)])
+
+        while queue:
+            node, currSum = queue.popleft()
+            # base condition
+            if not node.left and not node.right and currSum == 0:
+                return True
+            if node.left:
+                queue.append((node.left , currSum - node.left.val))
+            if node.right:
+                queue.append((node.right, currSum - node.right.val))
+        
+        return False
