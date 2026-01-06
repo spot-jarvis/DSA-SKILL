2d402c9dbcd2495e3284cc3ad2b45b6cbd5940ed
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-01-06 07:30:11 +0530

    combination sum using backtracking

--- trees/combinationSum.py
+++ trees/combinationSum.py
@@ -17,3 +17,27 @@ class Solution:
         
         dfs(0,[],0)
         return res
+    
+
+#optimal way by using sorting we can skip the unwanted iteration
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def dfs(i, curr, total):
+            if total == target:
+                res.append(curr.copy())
+                return 
+            
+            for j in range(i,len(nums)):
+                if nums[j] + total > target:
+                    return 
+                curr.append(nums[j])
+                dfs(j,curr,total+nums[j])
+                curr.pop()
+
+        dfs(0,[],0)
+        return res
+        
