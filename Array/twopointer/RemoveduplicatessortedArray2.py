ea0312567e7297963b29217acc1f1c530c640260
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-01-07 07:38:54 +0530

    another way

--- Array/twopointer/RemoveduplicatessortedArray2.py
+++ Array/twopointer/RemoveduplicatessortedArray2.py
@@ -7,3 +7,15 @@ class Solution:
                 j+=1
 
         return j
+    
+# another way
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return len(nums)
+        j = 2
+        for i in range(2,len(nums)):
+            if nums[i] != nums[j-2]:
+                nums[j] = nums[i]
+                j+=1
+        return j
