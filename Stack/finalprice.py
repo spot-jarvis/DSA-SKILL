b95aba3f702f75a3c057b3591f247b6ee1267627
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-02-09 10:56:52 +0530

    final prices with a special discount in shop

---
+++ Stack/finalprice.py
@@ -0,0 +1,15 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(prices)
+        for i in range(len(prices)-1, -1, -1):
+            while stack and prices[i] < stack[-1]:
+                stack.pop()
+            if stack:
+                res[i] = prices[i] - stack[-1]
+            else:
+                res[i] = prices[i]
+            
+            stack.append(prices[i])
+        
+        return res
